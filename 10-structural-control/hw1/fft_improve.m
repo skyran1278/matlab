d9ddb73c81f712b62d187a73d16d9fd2212fece2
skyran1278 <skyran1278@gmail.com>
2018-03-20 23:39:37 +0800

    sync

---
+++ 10-structural-control/hw1/fft_improve.m
@@ -0,0 +1,17 @@
+function [frequency, amplitude] = fft_improve(data, time_interval)
+
+  data_length = length(data);
+
+  data_fft = fft(data);
+
+  data_abs = abs(data_fft / data_length);
+
+  data_half = data_abs(1 : fix(data_length / 2 + 1));
+
+  data_half(2 : end - 1) = 2 * data_half(2 : end - 1);
+
+  amplitude = data_half;
+
+  frequency = 1 / time_interval * (0 : fix(data_length / 2) ) / data_length;
+
+end
