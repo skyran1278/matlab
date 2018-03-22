e6bcae13d5bfc7d07c2120904aeffa3426c3d582
skyran1278 <skyran1278@gmail.com>
2018-03-22 17:18:07 +0800

    sc sync

--- 10-structural-control/hw1/find_period.m
+++ 10-structural-control/hw1/find_period.m
@@ -1,7 +1,7 @@
 clc; clear; close all;
 
-El50AXBS_fileid = fopen('El50AXBS.txt', 'r');
-EL50RFA_fileid = fopen('EL50RFA.txt', 'r');
+El50AXBS_fileid = fopen('ETABS/El50AXBS.txt', 'r');
+EL50RFA_fileid = fopen('ETABS/EL50RFA.txt', 'r');
 
 El50AXBS = fscanf(El50AXBS_fileid, '%f');
 EL50RFA = fscanf(EL50RFA_fileid, '%f');
@@ -13,9 +13,14 @@ transfer_function = EL50RFA_ampl ./ El50AXBS_ampl;
 
 plot(El50AXBS_freq(2 : end), transfer_function(2 : end));
 
-[transfer_function_max, transfer_function_index]=max(transfer_function(2 : end));
+% findpeaks(transfer_function(2 : end), El50AXBS_freq(2 : end))
+[~, transfer_function_index_1]=max(transfer_function);
+transfer_function(transfer_function_index_1) = -Inf;
 
-1 / El50AXBS_freq(transfer_function_index)
+[~, transfer_function_index_2]=max(transfer_function);
+
+mode1 = 1 / El50AXBS_freq(transfer_function_index_2)
+mode2 = 1 / El50AXBS_freq(407)
 
 title('Transfer Function');
 xlabel('f (Hz)');
