ee5f5934bbceecfc684c224f46e0c16e9b6de0b9
skyran1278 <skyran1278@gmail.com>
2018-03-22 17:30:44 +0800

    sc modify homework

--- 10-structural-control/hw1/plot_damping.m
+++ 10-structural-control/hw1/plot_damping.m
@@ -1,9 +1,13 @@
 clc; clear; close all;
 
-find_damping('damping_2.7%.txt');
-find_damping('damping_2.5%.txt');
-find_damping('damping_2%.txt');
-find_damping('damping_3%.txt');
-find_damping('NCERB_ETABS.txt');
-find_damping('TzuChi_ETABS.txt');
-find_damping('damping_2.8%.txt');
+% find_damping('damping_2.7%.txt');
+% find_damping('damping_2.7%_modify.txt');
+% find_damping('damping_3%_modify.txt');
+find_damping('damping_2.9%_modify.txt');
+find_damping('damping_2.9%_modify_1.txt');
+% find_damping('damping_2.5%.txt');
+% find_damping('damping_2%.txt');
+% find_damping('damping_3%.txt');
+% find_damping('NCERB_ETABS.txt');
+% find_damping('TzuChi_ETABS.txt');
+% find_damping('damping_2.8%.txt');
