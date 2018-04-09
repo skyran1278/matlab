9c7770eb82d8c6ee7fd8432c9546da7cad50eaa1
skyran1278 <skyran1278@gmail.com>
2018-04-10 02:11:32 +0800

    fem hw3

--- 10-fem/hw3/hw3_2.m
+++ 10-fem/hw3/hw3_2.m
@@ -26,8 +26,6 @@ Ke = simplify(int(Be.' * Ae * Ee * Be, x, xe1, xe2));
 Ke1 = subs(Ke, [xe1 xe2], [0, 1])
 Ke2 = subs(Ke, [xe1 xe2], [1, 2])
 
-
-
 b(x) = unit_weighs * (x + 1);
 
 fe_omega = simplify(int(Ne.' * b(x), x, xe1, xe2));
@@ -37,38 +35,39 @@ fe_gamma = subs(Ne.' * Ae * t_bar, x, 0);
 fe1 = subs(fe_omega + fe_gamma, [xe1 xe2], [0, 1])
 fe2 = subs(fe_omega, [xe1 xe2], [1, 2])
 
-K = zeros(3);
+Kg = zeros(3);
 
 for index = 1 : 2
-    K([index, index + 1], [index, index + 1]) = K([index, index + 1], [index, index + 1]) + eval(['Ke' num2str(index)]);
+    Kg([index, index + 1], [index, index + 1]) = Kg([index, index + 1], [index, index + 1]) + eval(['Ke' num2str(index)]);
 end
 
-K
+Kg
 
-f = zeros(3, 1);
+fg = zeros(3, 1);
 
 for index = 1 : 2
-    f([index, index + 1], 1) = f([index, index + 1], 1) + eval(['fe' num2str(index)]);
+    fg([index, index + 1], 1) = fg([index, index + 1], 1) + eval(['fe' num2str(index)]);
 end
 
-f
+fg
 
-d = zeros(3, 1)
-d([1 2], 1) = K([1 2], [1 2]) \ f([1 2], 1)
+dg = zeros(3, 1);
 
-r = K * d - f
+dg([1 2], 1) = Kg([1 2], [1 2]) \ fg([1 2], 1);
+
+r = Kg * dg - fg;
 
 
-t = 0 : 0.01 : 2;
+x1 = 0 : 0.01 : 2;
 
-u_linear = 10^-7 * (30 - 15 * t);
+u_linear = 10^-7 * (30 - 15 * x1);
 
-u_exact = (-6 * (t + 1).^2 - 8 * log(t + 1) + 54 + 8 * log(3)) / (2 * 10^7);
+u_exact = (-6 * (x1 + 1).^2 - 8 * log(x1 + 1) + 54 + 8 * log(3)) / (2 * 10^7);
 
-x = [0; 1; 2];
+x2 = [0; 1; 2];
 
 figure;
-plot(t, u_linear, t, u_exact, x, d);
+plot(x1, u_linear, x1, u_exact, x2, dg);
 legend('linear', 'exact', 'fem');
 xlabel('x (m)');
 ylabel('disp (m)');
