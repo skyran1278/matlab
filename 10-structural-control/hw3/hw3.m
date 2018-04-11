02d1b905e553c29e2433252eb35f31ee1f10fccb
skyran1278 <skyran1278@gmail.com>
2018-04-11 19:33:06 +0800

    pc hw2

--- 10-structural-control/hw3/hw3.m
+++ 10-structural-control/hw3/hw3.m
@@ -11,28 +11,34 @@ time_interval = 0.005;
 
 damping_ratios = [0.02 0.05 0.1 0.2 0.4];
 
-% tn = 0 : 0.01 : 10;
+tn_data = 0 : 0.1 : 10;
+tn_length = length(tn_data);
+p_sa = zeros(tn_length, 1);
+sa = zeros(tn_length, 1);
 
+for damping_index = 1 : 5
 
-% wn = (2 * pi) ./ tn;
-% wd = wn .* sqrt(1 - (damping_ratios .^ 2));
+    damping_ratio = damping_ratios(damping_index);
 
-% hw = - 1 / wd .* exp(- damping_ratios .* wn .* time_data) .* sin(wd .* time_data);
+    for tn_index = 1 : tn_length
 
-% u = convn(ew_data, hw) * time_interval;
-
-for index = 1 : 5
-
-    damping_ratio = damping_ratios(index)
-
-    for tn = 0.01 : 0.01 : 10
+        tn = tn_data(tn_index);
 
         wn = (2 * pi) / tn;
         wd = wn * sqrt(1 - (damping_ratio ^ 2));
 
         hw = - 1 / wd * exp(- damping_ratio * wn * time_data) .* sin(wd * time_data);
 
-        u = convn(ew_data, hw) * time_interval;
+        u = conv(ew_data, hw) * time_interval;
+
+        diff_hw = - exp(- damping_ratio * wn * time_data) .* cos(wd * time_data);
+
+        c = conv(ew_data, diff_hw) * time_interval;
+
+        v = - damping_ratio * wn * u + c;
+
+        a = - wn ^ 2 * u - 2 * damping_ratio * wn * v;
+        p_sa()
 
     end
 end
