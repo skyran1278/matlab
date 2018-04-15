77ae5de97576f3e8e790bff32034f4df414ac3a5
skyran1278 <skyran1278@gmail.com>
2018-04-15 21:55:39 +0800

    pc hw3

--- 10-structural-control/hw3/hw3.m
+++ 10-structural-control/hw3/hw3.m
@@ -5,6 +5,7 @@ filename = ["TCU003" "TCU006" "TCU007" "TCU008" "TCU009" "TCU010" "TCU011" "TCU0
 time_interval = 0.005;
 
 damping_ratios = [0.02 0.05 0.1 0.2];
+damping_ratios_name = ["\xi = 2%", "\xi = 5%", "\xi = 10%", "\xi = 20%"];
 
 periods = [0.2 1 2 4];
 
@@ -15,7 +16,11 @@ periods_length = length(periods);
 damping_ratio_5_percent = 0.05;
 
 sa_5_percent = zeros(periods_length, filename_length);
+sd_5_percent = zeros(periods_length, filename_length);
 sa = zeros(periods_length, filename_length);
+sd = zeros(periods_length, filename_length);
+ba_mean = zeros(periods_length, damping_ratios_length);
+bd_mean = zeros(periods_length, damping_ratios_length);
 
 for period_index = 1 : periods_length
 
@@ -25,9 +30,10 @@ for period_index = 1 : periods_length
 
         ag = output_ag_by_filename(filename(filename_index));
 
-        [~, ~, a] = newmark_beta(ag, time_interval, damping_ratio_5_percent, tn, 'average');
+        [u, ~, a] = newmark_beta(ag, time_interval, damping_ratio_5_percent, tn, 'average');
 
         sa_5_percent(period_index, filename_index) = max(abs(a));
+        sd_5_percent(period_index, filename_index) = max(abs(u));
 
     end
 
@@ -45,22 +51,48 @@ for damping_index = 1 : damping_ratios_length
 
             ag = output_ag_by_filename(filename(filename_index));
 
-            [~, ~, a] = newmark_beta(ag, time_interval, damping_ratio, tn, 'average');
+            [u, ~, a] = newmark_beta(ag, time_interval, damping_ratio, tn, 'average');
 
             sa(period_index, filename_index) = max(abs(a));
+            sd(period_index, filename_index) = max(abs(u));
 
         end
 
     end
 
-    figure(damping_index);
-    plot(periods.' * ones(1, filename_length), sa ./ sa_5_percent, 'o');
+    ba = sa ./ sa_5_percent;
+    bd = sd ./ sd_5_percent;
+
+    ba_mean(:, damping_index) = mean(ba, 2);
+    bd_mean(:, damping_index) = mean(bd, 2);
+
+    figure;
+    plot(periods, ba, 'o');
     title(['Damping Reduction Factors Ba \xi = ' num2str(damping_ratio)]);
     xlabel('Period (s)');
     ylabel('Ba');
 
+    figure;
+    plot(periods, ba, 'o');
+    title(['Damping Reduction Factors Bd \xi = ' num2str(damping_ratio)]);
+    xlabel('Period (s)');
+    ylabel('Bd');
+
 end
 
+figure;
+plot(periods, ba_mean, 'o');
+title('Ba average');
+legend(damping_ratios_name.', 'Location', 'northeast');
+xlabel('Period (s)');
+ylabel('Ba');
+
+figure;
+plot(periods, ba_mean, 'o');
+title('Bd average');
+legend(damping_ratios_name.', 'Location', 'northeast');
+xlabel('Period (s)');
+ylabel('Bd');
 
 
 function [u, v, a] = newmark_beta(ag, time_interval, damping_ratio, tn, method)
