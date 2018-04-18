c83cff5932a0beb218a533a87b8380dae0b18d41
skyran1278 <skyran1278@gmail.com>
2018-04-18 17:03:33 +0800

    pc hw4

--- 10-structural-control/hw4/filename_to_ag.m
+++ 10-structural-control/hw4/filename_to_ag.m
@@ -2,7 +2,7 @@ function ag = filename_to_ag(filename, ag_col)
 %
 % 適用於被動控制給的 file_input.
 %
-% @since 1.1.0
+% @since 1.0.0
 % @param {string} [filename] 檔案名稱.
 % @param {number} [ag_col] 要回傳第幾欄.
 % @return {array} [ag] 加速度歷時資料.
---
+++ 10-structural-control/hw4/hw4.m
@@ -0,0 +1,27 @@
+clc; clear; close all;
+
+ag = filename_to_ag('TCU052', 4);
+
+time_interval = 0.005;
+
+damping_ratio = 0.05;
+
+tn = 0.1;
+
+method = 'average';
+
+m = 500;
+
+c = 2 * damping_ratio * (2 * pi) / tn * m;
+
+k = 1973921;
+
+[u, v, a] = newmark_beta(ag, time_interval, damping_ratio, tn, method, m, c, k);
+
+kinetic_energy = 1 / 2 * m * (v .^ 2);
+
+potential_energy = 1 / 2 * k * (u .^ 2);
+
+modal_damping_energy = c * (v .^ 2);
+
+input_energy = kinetic_energy + potential_energy + modal_damping_energy;
