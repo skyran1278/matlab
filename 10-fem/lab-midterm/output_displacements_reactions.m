a0018fd09d70f2e060cbdae304a5cca2d0fa3329
skyran1278 <skyran1278@gmail.com>
2018-04-24 23:56:20 +0800

    fem midterm

--- 10-fem/lab-midterm/output_displacements_reactions.m
+++ 10-fem/lab-midterm/output_displacements_reactions.m
@@ -1,27 +1,31 @@
-function [] = output_displacements_reactions(displacements, stiffness, GDof, prescribedDof, force)
+function [] = output_displacements_reactions(displacements, stiffness, G_dof, prescribedDof, force)
+%
+% output of displacements and reactions in tabular form.
+%
+% @since 1.0.0
+% @param {array} [displacements] displacements.
+% @param {array} [stiffness] stiffness.
+% @param {number} [G_dof] total number of degrees of freedom.
+% @param {array} [prescribed_dof] 有束制的節點.
+% @param {array} [force] force.
+%
 
-    % output of displacements and reactions in
-    % tabular form
-
-    % GDof: total number of degrees of freedom of
-    % the problem
-
-    disp('Displacements')
-    fprintf('node     displacements\n')
+    disp('Displacements');
+    fprintf('node     displacements\n');
 
     % displacements
-    for jj = 1 : GDof
-        fprintf('%2.0f:        %10.4e\n', jj, displacements(jj))
+    for index = 1 : G_dof
+        fprintf('%2.0f:        %10.4e\n', index, displacements(index));
     end
 
     % reactions
     F = stiffness * displacements;
     reactions = F(prescribedDof) - force(prescribedDof);
 
-    disp('Reactions')
-    fprintf('node     reactions\n')
-    for jj = 1 : size(prescribedDof, 1)
-        fprintf('%2.0f:        %10.4e\n', prescribedDof(jj), reactions(jj))
+    disp('Reactions');
+    fprintf('node     reactions\n');
+    for index = 1 : size(prescribedDof, 1)
+        fprintf('%2.0f:        %10.4e\n', prescribedDof(index), reactions(index));
     end
 
 end
