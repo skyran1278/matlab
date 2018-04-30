c2df1025ce810dcb6d311f35111cfe147f6938e1
skyran1278 <skyran1278@gmail.com>
2018-04-30 12:20:49 +0800

    utils fem

--- utils/fem/solution.m
+++ utils/fem/solution.m
@@ -2,7 +2,7 @@ function displacements = solution(G_dof, prescribed_dof, stiffness, force, displ
 %
 % displacements = inv(stiffness) / force.
 %
-% @since 1.0.1
+% @since 1.0.2
 % @param {number} [G_dof] total number of degrees of freedom.
 % @param {array} [prescribed_dof] 有束制的節點.
 % @param {array} [stiffness] stiffness.
@@ -15,11 +15,11 @@ function displacements = solution(G_dof, prescribed_dof, stiffness, force, displ
     active_dof = setdiff((1 : G_dof)', prescribed_dof);
 
     % 這裡的感覺應該是束制的 displacements，對於沒束制的影響
-    % 類似 fef
-    eq_force = stiffness(prescribed_dof, active_dof).' * displacements(prescribed_dof);
+    % fef
+    fef = stiffness(prescribed_dof, active_dof).' * displacements(prescribed_dof);
 
     % 只計算沒束制的自由度
-    U = stiffness(active_dof, active_dof) \ (force(active_dof) - eq_force);
+    U = stiffness(active_dof, active_dof) \ (force(active_dof) - fef);
 
     displacements(active_dof) = U;
 
