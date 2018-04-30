47fbb38b5d014b1a947a67c9d4ad0f969ed756b0
skyran1278 <skyran1278@gmail.com>
2018-04-30 10:39:21 +0800

    st hw4

---
+++ 10-steel/hw4/angle_to_radian.m
@@ -0,0 +1,13 @@
+function radian = angle_to_radian(angle_)
+%
+% 角度轉換成弧度.
+%
+% @since 1.0.0
+% @param {number} [angle_] 角度.
+% @return {number} [radian] 弧度.
+%
+
+    radian = angle_ * pi / 180;
+
+end
+
