8b98605446cac436e02cbf439d6cfec8f1c3e054
skyran1278 <skyran1278@gmail.com>
2018-05-01 23:05:18 +0800

    fem hw6

--- 10-fem/hw6/hw6.m
+++ 10-fem/hw6/hw6.m
@@ -1,6 +1,7 @@
 clc; clear; close all;
 
-syms E v sigma_xx sigma_yy sigma_zz sigma_xy sigma_yz sigma_zx epsilon_xx epsilon_yy epsilon_zz gamma_xy gamma_yz gamma_zx;
+syms E v;
+% syms sigma_xx sigma_yy sigma_zz sigma_xy sigma_yz sigma_zx epsilon_xx epsilon_yy epsilon_zz gamma_xy gamma_yz gamma_zx;
 
 D = E / ((1 + v) * (1 - 2 * v)) * [
     (1 - v), v, v, 0, 0, 0;
@@ -11,9 +12,9 @@ D = E / ((1 + v) * (1 - 2 * v)) * [
     0, 0, 0, 0, 0, (1 - 2 * v) / 2;
 ];
 
-sigma = [sigma_xx; sigma_yy; sigma_zz; sigma_xy; sigma_yz; sigma_zx];
+% sigma = [sigma_xx; sigma_yy; sigma_zz; sigma_xy; sigma_yz; sigma_zx];
 
-epsilon(epsilon_xx, epsilon_yy, epsilon_zz, gamma_xy, gamma_yz, gamma_zx) = [epsilon_xx; epsilon_yy; epsilon_zz; gamma_xy; gamma_yz; gamma_zx];
+% epsilon(epsilon_xx, epsilon_yy, epsilon_zz, gamma_xy, gamma_yz, gamma_zx) = [epsilon_xx; epsilon_yy; epsilon_zz; gamma_xy; gamma_yz; gamma_zx];
 
 condense_factor = [
     1 0 0 0 0 0;
