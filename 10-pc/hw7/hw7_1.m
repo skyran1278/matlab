2a3cdf87cceb91b7d588d4ea31639ac7671b4a0f
skyran1278 <skyran1278@gmail.com>
2018-06-05 17:54:56 +0800

    pc hw7

---
+++ 10-pc/hw7/hw7_1.m
@@ -0,0 +1,23 @@
+clc; clear; close all;
+
+ag = filename_to_array('I-ELC270_gal_l00Hz', 2, 2);
+
+tn = 0.01 : 0.01 : 5;
+tn_length = length(tn);
+acceleration = zeros(1, tn_length);
+
+for index = 1 : tn_length
+
+    [~, ~, a_array] = newmark_beta(ag, 0.01, 0.05, tn(index), 'average');
+
+    acceleration(1, index) = max(abs(a_array));
+
+end
+
+acceleration_normal = acceleration / acceleration(1, 1) * 0.4;
+
+figure;
+plot(tn, acceleration_normal);
+title('');
+xlabel('');
+ylabel('');
---
+++ 10-pc/hw7/newmark_beta.m
@@ -0,0 +1,58 @@
+function [u, v, a] = newmark_beta(ag, time_interval, damping_ratio, tn, method)
+%
+% newmark beta.
+%
+% @since 1.0.0
+% @param {array} [ag] input 的地震力歷時資料.
+% @param {number} [time_interval] 時間間隔.
+% @param {number} [damping_ratio] 阻尼比.
+% @param {number} [tn] 自然週期.
+% @param {string} [method] 'average' or 'linear'.
+% @return {array} [u] 位移.
+% @return {array} [v] 速度.
+% @return {array} [a] 加速度.
+% @see newmark_beta_calculation
+%
+
+    % average Acceleration Method
+    gamma_ = 1 / 2;
+
+    switch method
+
+        % average Acceleration Method
+        case 'average'
+            beta_ = 1 / 4;
+
+        % linear Acceleration Method
+        case 'linear'
+            beta_ = 1 / 6;
+
+    end
+
+    wn = (2 * pi) / tn;
+
+    m = 1;
+    c = 2 * damping_ratio * wn * m;
+    k = (wn ^ 2) * m;
+
+    p_t = - m * ag;
+
+    u = zeros(size(p_t));
+    v = zeros(size(p_t));
+    a = zeros(size(p_t));
+
+    a(1) = 1 / m * (p_t(1) - c * v(1) - k * u(1));
+
+    ag_length = length(ag);
+
+    for ag_index = 2 : ag_length
+
+        dp = p_t(ag_index) - p_t(ag_index - 1);
+
+        [u(ag_index), v(ag_index), a(ag_index)] = newmark_beta_calculation(m, c, k, u(ag_index - 1), v(ag_index - 1), a(ag_index -1), dp, time_interval, gamma_, beta_);
+
+    end
+
+    a = a + ag;
+
+end
