c4e179270de9870387d1ffdee6a8a858cafccb20
skyran1278 <skyran1278@gmail.com>
2018-06-10 02:17:37 +0800

    st dp4

--- 10-st/DP4/matlab/dp4_2.m
+++ 10-st/DP4/matlab/dp4_2.m
@@ -4,17 +4,52 @@ period = filename_to_array('base_shaer_x', 2, 1, 1) / 160;
 base_shaer_x = filename_to_array('base_shaer_x', 2, 2, 1);
 base_shaer_z = filename_to_array('base_shaer_z', 2, 2, 1);
 
+base_shaer_x_diff1 = abs(diff(base_shaer_x, 1));
+base_shaer_x_diff2 = abs(diff(base_shaer_x, 2));
+base_shaer_x_diff3 = abs(diff(base_shaer_x, 3));
+
+% figure;
+% plot(period(1 : end - 1), base_shaer_x_diff1);
+
+% figure;
+% plot(period(1 : end - 2), base_shaer_x_diff2);
+
+% figure;
+% plot(period(1 : end - 3), base_shaer_x_diff3);
+
+design_base_shear_x = base_shaer_x(base_shaer_x_diff2 > 1);
+
 
 figure;
-plot(period, base_shaer_x);
+plot(period, base_shaer_x, [0 2.5], [design_base_shear_x(1) design_base_shear_x(1)], 'k--', [0 2.5], [base_shaer_x(end) base_shaer_x(end)], 'k--', [0.25 0.25], [base_shaer_x(end), design_base_shear_x(1)], ':');
+text(1, design_base_shear_x(1) - 1000, ['static LRFD design base shear = ' num2str(design_base_shear_x(1)) ' kN']);
+text(1, base_shaer_x(end) + 500, ['ultimate strength = ' num2str(base_shaer_x(end)) ' kN']);
+text(0.3, (base_shaer_x(end) + design_base_shear_x(1)) / 2, ['\Omega_0 = ' num2str(base_shaer_x(end) / design_base_shear_x(1))]);
 title('Pushover Curve X direction');
 xlabel('Drift Ratio(%)');
 ylabel('Base Shear(kN)');
 
+
+base_shaer_z_diff1 = abs(diff(base_shaer_z, 1));
+base_shaer_z_diff2 = abs(diff(base_shaer_z, 2));
+base_shaer_z_diff3 = abs(diff(base_shaer_z, 3));
+
+% figure;
+% plot(period(1 : end - 1), base_shaer_z_diff1);
+
+% figure;
+% plot(period(1 : end - 2), base_shaer_z_diff2);
+
+% figure;
+% plot(period(1 : end - 3), base_shaer_z_diff3);
+
+design_base_shear_z = base_shaer_z(base_shaer_z_diff2 > 1);
+
 figure;
-plot(period, base_shaer_z);
+plot(period, base_shaer_z, [0 2.5], [design_base_shear_z(1) design_base_shear_z(1)], 'k--', [0 2.5], [base_shaer_z(end) base_shaer_z(end)], 'k--', [0.25 0.25], [base_shaer_z(end), design_base_shear_z(1)], ':');
+text(1, design_base_shear_z(1) - 1000, ['static LRFD design base shear = ' num2str(design_base_shear_z(1)) ' kN']);
+text(1, base_shaer_z(end) - 500, ['ultimate strength = ' num2str(base_shaer_z(end)) ' kN']);
+text(0.3, (base_shaer_z(end) + design_base_shear_z(1)) / 2 + 1000, ['\Omega_0 = ' num2str(base_shaer_z(end) / design_base_shear_z(1))]);
 title('Pushover Curve Y direction');
 xlabel('Drift Ratio(%)');
 ylabel('Base Shear(kN)');
-
-
---
+++ 10-st/DP4/matlab/dp4_3.m
@@ -0,0 +1,29 @@
+clc; clear; close all;
+
+spectra_period = filename_to_array('spectra', 2, 1);
+spectra = filename_to_array('spectra', 2, 2) / 0.32;
+
+period_sp = filename_to_array('CHY076SN_sp', 3, 1, 1);
+CHY076SN_sp = filename_to_array('CHY076SN_sp', 3, 2, 1);
+CHY076EW_sp = filename_to_array('CHY076EW_sp', 3, 2, 1);
+
+T1x = 1.0118E+000;
+T2x = 3.1046E-001;
+T1y = 5.1241E-001;
+T2y = 1.8692E-001;
+
+figure;
+plot(spectra_period, spectra, period_sp, CHY076EW_sp, [T1x T1x], [0 4], [T2x T2x], [0 4]);
+title('Response Spectrum in X Direction');
+xlabel('Period(sec)');
+ylabel('SaD(g)');
+axis([0 3 0 4]);
+legend('design spectra', '5% damping normalized response spectra', ['T1x = ' num2str(T1x)], ['T2x = ' num2str(T2x)], 'Location', 'northeast');
+
+figure;
+plot(spectra_period, spectra, period_sp, CHY076SN_sp, [T1y T1y], [0 4], [T2y T2y], [0 4]);
+title('Response Spectrum in X Direction');
+xlabel('Period(sec)');
+ylabel('SaD(g)');
+axis([0 3 0 4]);
+legend('design spectra', '5% damping normalized response spectra', ['T1y = ' num2str(T1y)], ['T2y = ' num2str(T2y)], 'Location', 'northeast');
