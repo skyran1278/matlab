42dab2bd8e65ba28223ea5893160504dad3031ac
skyran1278 <skyran1278@gmail.com>
2018-06-20 19:03:24 +0800

    pc

--- 10-pc/dp1/matlab/FPS_FNA.m
+++ 10-pc/dp1/matlab/FPS_FNA.m
@@ -4,8 +4,13 @@ centerHysteresis = xlsread('FPS_center.xlsx', 1, 'C:D');
 FNAHysteresis = xlsread('FPS_FNA.xlsx', 1, 'C:D');
 % edge1Hysteresis = xlsread('FPS_edge1.xlsx', 1, 'C:D');
 % edge2Hysteresis = xlsread('FPS_edge2.xlsx', 1, 'C:D');
+figure;
+plot(FNAHysteresis(:, 1), FNAHysteresis(:, 2), 'r-');
+xlabel('displacement(m)');
+ylabel('force(tf)');
+figure;
+plot(centerHysteresis(:, 1), centerHysteresis(:, 2));
 
-plot(centerHysteresis(:, 1), centerHysteresis(:, 2), FNAHysteresis(:, 1), FNAHysteresis(:, 2));
-legend('center', 'FNA');
+% legend('center', 'FNA');
 xlabel('displacement(m)');
 ylabel('force(tf)');
