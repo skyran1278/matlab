7073e15ea809901c496d37380ba7dbbe6fb6195a
skyran1278 <skyran1278@gmail.com>
2018-09-26 15:06:22 +0800

    pa hw1

--- 10-PA/hw1.m
+++ 10-PA/hw1.m
@@ -1,11 +1,11 @@
 clc; clear; close all;
 
 
-alpha_ = 0 : 0.01 : 4;
+alpha_ = 0 : 0.3 : 4;
 beta_ = 0.5;
 alphaLength = length(alpha_);
 
-xi = (1 + alpha_ .^ 2 * beta_) / (3 + 2 * alpha_);
+xi = (1 + alpha_ .^ 2 * beta_) ./ (3 + 2 * alpha_);
 
 Mc = abs(-3 / 2 * xi);
 Mb = abs(alpha_ * beta_ - 3 / 4 * xi);
@@ -17,12 +17,12 @@ MbHinge = abs(alpha_ * beta_ - 1 / 4);
 
 figure;
 hold on;
-plot(alpha_, Mc);
-plot(alpha_, Mb);
-plot(alpha_, Md);
-plot(alpha_, McHinge);
-plot(alpha_, MdHinge);
-plot(alpha_, MbHinge);
+plot(alpha_, Mc, '-o');
+plot(alpha_, Mb, '-+');
+plot(alpha_, Md, '-*');
+plot(alpha_, McHinge, ':o');
+plot(alpha_, MdHinge, ':+');
+plot(alpha_, MbHinge, ':*');
 legend('MB','MC','MD','MB-hinge','MC-hinge','MD-hinge','Location','northwest');title('');
 xlabel('');
 ylabel('');
